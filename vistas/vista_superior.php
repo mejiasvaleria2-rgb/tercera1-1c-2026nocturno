056ece241ce5b8779e5304752846c7c368bda89c
valeria mejias <mejiasvaleria2@gmail.com>
2026-06-07 20:29:43 -0600

    lección 2

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tercera 1</title>
+    <title> Tercera 1</title>
 </head>
 <link rel="stylesheet" href="./css/main.css">
 <body>
    <header>
     <menu>
     <div class = "menu-izquierda">
-        <h1> Tercera 1</h1>
+        <h1> Grotescas </h1>
         <img src="https://i.pinimg.com/1200x/cd/86/f2/cd86f270bb6d37d996af8bc15d1a77f8.jpg" alt="logo" 
-        width="250px" height="230px"> 
+        width="250px" height="210px"> 
 </div>
     <div class = "menu-derecha">
         <a href="./inicio.php">Inicio</a>

6e714fbb2de024cfe165b110f8575c44fc9b6814
valeria mejias <mejiasvaleria2@gmail.com>
2026-06-10 16:18:51 -0600

    Ajuste de imágenes

--- fotos.php
+++ fotos.php
@@ -1,5 +1,13 @@
-<?php  require_once "./vistas/vista_superior.php"?>
+<?php require_once "./vistas/vista_superior.php" ?>
 
-<h1> fotos </h1> 
+<main>
+    <h2>Diseños</h2>
 
-<?php  require_once "./vistas/vista_inferior.php"?>
+    <div class="galeria">
+        <img src="./img/diseno_1.jpg" alt="Diseño 1">
+        <img src="./img/diseno_2.jpg" alt="Diseño 2">
+        <img src="./img/diseno_3.jpg" alt="Diseño 3">
+    </div>
+</main>
+
+<?php require_once "./vistas/vista_inferior.php" ?>

92261baff8ea660240613fe35a8d76434401c63d
valeria mejias <mejiasvaleria2@gmail.com>
2026-06-09 22:02:42 -0600

    Leccion_3s

--- contactenos.php
+++ contactenos.php
@@ -1,5 +1,7 @@
+
 <?php  require_once "./vistas/vista_superior.php"?>
 
 <h1> contactenos </h1>
 
-<?php  require_once "./vistas/vista_inferior.php"?>
+<?php  require_once "./vistas/vista_inferior.php"?> 
+
--- fotos.php
+++ fotos.php
@@ -1,5 +1,5 @@
 <?php  require_once "./vistas/vista_superior.php"?>
 
-<h1> Fotos </h1>
+<h1> fotos </h1> 
 
 <?php  require_once "./vistas/vista_inferior.php"?>
